580a03fdff9e11814fa63eb9ca6c186c0318a827
Sergey Golenko <serjgolenko@gmail.com>
2019-01-24 21:10:22 +0200

    sql connection changes added

--- src/com/golenko/gameshop/dao/MySqlConnection.java
+++ src/com/golenko/gameshop/dao/MySqlConnection.java
@@ -9,15 +9,17 @@ import java.sql.DriverManager;
 
 public class MySqlConnection {
 	private static Connection CONNECTION;
-	private static String URL = "jdbc:mysql://localhost/gameshop";
+	private static String URL = "jdbc:mysql://localhost:3306/gameshop";
 	private static String USERNAME = "root";
 	private static String PASSWORD = "";
 
 	static {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            System.out.println("Connection succesfull!");;
 
 		} catch (Exception e) {
+			 System.out.println("Connection failed!");; 	
 			System.out.println(e.getMessage());
 			
 		}
